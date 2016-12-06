b0d6e03a63b73c60a8955b4608737d99fba34bd3
Célina Ngapy <Clina Ngapy@DESKTOP-KSOKUS3>
2016-12-06 10:32:59 +0100

    Message aangepast

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
+            MessageBox.Show("Je Moeder");
         }
 
      
