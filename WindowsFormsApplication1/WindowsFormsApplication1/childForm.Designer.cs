6ef0ad756bbf5535b084e8ff786fffdefa7036a2
Célina Ngapy <Clina Ngapy@DESKTOP-KSOKUS3>
2016-12-22 19:35:25 +0100

    childForm verandert(datepicker, labels en listboxes toegevoegd)

--- WindowsFormsApplication1/WindowsFormsApplication1/childForm.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/childForm.Designer.cs
@@ -32,58 +32,100 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.lbOnTime = new System.Windows.Forms.ListBox();
+            this.lbTooLate = new System.Windows.Forms.ListBox();
+            this.lbUpcoming = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(29, 25);
+            this.dateTimePicker1.Font = new System.Drawing.Font("Poppins", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(123, 38);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(253, 35);
             this.dateTimePicker1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 74);
+            this.label1.Font = new System.Drawing.Font("Poppins", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 119);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.Size = new System.Drawing.Size(71, 28);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Aantal keer op tijd:";
+            this.label1.Text = "Op tijd:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 106);
+            this.label2.Font = new System.Drawing.Font("Poppins", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(283, 119);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(127, 17);
+            this.label2.Size = new System.Drawing.Size(73, 28);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Aantal keer te laat:";
+            this.label2.Text = "Te laat:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(183, 74);
+            this.label3.Font = new System.Drawing.Font("Poppins", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 37);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.Size = new System.Drawing.Size(63, 28);
             this.label3.TabIndex = 3;
-            this.label3.Text = "7";
+            this.label3.Text = "Week:";
+            // 
+            // lbOnTime
+            // 
+            this.lbOnTime.Font = new System.Drawing.Font("Poppins", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOnTime.FormattingEnabled = true;
+            this.lbOnTime.ItemHeight = 21;
+            this.lbOnTime.Location = new System.Drawing.Point(38, 160);
+            this.lbOnTime.Name = "lbOnTime";
+            this.lbOnTime.Size = new System.Drawing.Size(197, 277);
+            this.lbOnTime.TabIndex = 4;
+            // 
+            // lbTooLate
+            // 
+            this.lbTooLate.Font = new System.Drawing.Font("Poppins", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTooLate.FormattingEnabled = true;
+            this.lbTooLate.ItemHeight = 21;
+            this.lbTooLate.Location = new System.Drawing.Point(286, 160);
+            this.lbTooLate.Name = "lbTooLate";
+            this.lbTooLate.Size = new System.Drawing.Size(197, 277);
+            this.lbTooLate.TabIndex = 5;
+            // 
+            // lbUpcoming
+            // 
+            this.lbUpcoming.Font = new System.Drawing.Font("Poppins", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUpcoming.FormattingEnabled = true;
+            this.lbUpcoming.ItemHeight = 21;
+            this.lbUpcoming.Location = new System.Drawing.Point(539, 160);
+            this.lbUpcoming.Name = "lbUpcoming";
+            this.lbUpcoming.Size = new System.Drawing.Size(197, 277);
+            this.lbUpcoming.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(183, 106);
+            this.label4.Font = new System.Drawing.Font("Poppins", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(536, 119);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(16, 17);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "4";
+            this.label4.Size = new System.Drawing.Size(125, 28);
+            this.label4.TabIndex = 6;
+            this.label4.Text = " Aankomend:";
             // 
             // childForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(555, 360);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(795, 488);
+            this.Controls.Add(this.lbUpcoming);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbTooLate);
+            this.Controls.Add(this.lbOnTime);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -101,6 +143,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbOnTime;
+        private System.Windows.Forms.ListBox lbTooLate;
+        private System.Windows.Forms.ListBox lbUpcoming;
         private System.Windows.Forms.Label label4;
     }
 }
