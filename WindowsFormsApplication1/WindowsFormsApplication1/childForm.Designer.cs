0b7cb4420709597c9f0b66f06714dd92c4692e83
bheuvelings <b.heuvelings@me.com>
2017-01-16 21:23:07 +0100

    Minor changes

--- WindowsFormsApplication1/WindowsFormsApplication1/childForm.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/childForm.Designer.cs
@@ -37,6 +37,7 @@
             this.lbUpcoming = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dateTimePicker1
@@ -73,9 +74,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(35, 37);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.Size = new System.Drawing.Size(74, 20);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Week:";
+            this.label3.Text = "Welkom ";
             // 
             // lbOnTime
             // 
@@ -130,12 +131,23 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(35, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Week:";
+            // 
             // childForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(795, 487);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lbUpcoming);
             this.Controls.Add(this.label4);
@@ -165,5 +177,6 @@
         private System.Windows.Forms.ListBox lbUpcoming;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/childForm.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/childForm.cs
@@ -13,13 +13,14 @@ namespace WindowsFormsApplication1
 {
     public partial class childForm : Form
     {
-
+        
         string connectionString = @"Server=tcp:taskm8database.database.windows.net,1433;Initial Catalog=Proftaak_P2;Persist Security Info=False;User ID=taskM8;Password=Welkom00;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
+        
 
         public childForm()
         {
             InitializeComponent();
+            
         }
 
         string toViewDate;
@@ -34,25 +35,8 @@ namespace WindowsFormsApplication1
             Application.Exit();
         }
 
-        /*private void getChildInfo()
-        {
-
-            
-            
-            SqlConnection conn = new SqlConnection(connectionString);
-            string result = @"SELECT * FROM Vaat WHERE Datum = '" + dateTimePicker1.Value.ToString() + "';";
-            conn.Open();
-            SqlCommand showresult = new SqlCommand(result, conn);
-            SqlDataReader data = showresult.ExecuteReader();
-            lbOnTime.Items.Add(data["Vaat_optijd"].ToString());
-            lbTooLate.Items.Add(data["Vaat_TeLaat"].ToString());
-            lbUpcoming.Items.Add(data["Vaat_aankomend"].ToString());
-            conn.Close();
-        }*/
-
         private void childForm_Load(object sender, EventArgs e)
         {
-            //getChildInfo();
         }
 
         private void button1_Click(object sender, EventArgs e)
